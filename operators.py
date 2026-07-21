d70cfb89b00061596b1685fb363b965eb7f8df10
Manasvi <manasvigupta975@gmail.com>
2026-07-21 21:27:10 +0530

    relational_logical_assignment

---
+++ operators.py
@@ -0,0 +1,20 @@
+#relational operators
+num1=50
+num2=25
+print(num1 == num2)
+print(num1 != num2)
+print(num1 >= num2)
+print(num1 > num2)
+print(num1 <= num2)
+print(num1 < num2)
+#assignment operators
+a=60
+a += 5
+print("a:  ",a)
+
+#logical  operators
+a1 = True
+a2 = False
+print(not a1)
+print("and: ",a1 and a2)
+print("or: ",a1 or a2)
