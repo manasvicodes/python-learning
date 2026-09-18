152a0d383955231a2399a96b8ad673b9c18e8ea7
Manasvi <manasvigupta975@gmail.com>
2026-09-18 20:07:46 +0530

    statements

---
+++ statements.py
@@ -0,0 +1,11 @@
+light=("red")
+
+if(light=="green"):
+   print("go")
+elif(light=="red"):
+   print("stop")   
+elif(light=="yellow"):
+   print("look")   
+else :
+   print("light broken")
+   
--- str_function.py
+++ str_function.py
@@ -1,6 +1,2 @@
-str =("hey, manasvi here")
-print(str.endswith("ere"))
-print(str.capitalize())
-print(str.count("e"))
-print(str.replace("hey","hi"))
-print(str.find("a"))
+name=(input("enter your name = "))
+print("length of your name",len(name))
