4f8bfc8895b0eb6d490a4ea6bb2d11f879b8b584
Manasvi <manasvigupta975@gmail.com>
2026-09-22 19:03:47 +0530

    list methods

---
+++ list_methods.py
@@ -0,0 +1,6 @@
+list=[1,2,3]
+list.append(4)
+list.sort
+list.sort(reverse=True)
+list.insert(1,2)
+print(list)
--- statements.py
+++ statements.py
@@ -1,11 +1,12 @@
-light=("red")
+a = int(input(" enter number = "))
+b = int(input(" enter number = "))
+c = int(input(" enter number = "))
 
-if(light=="green"):
-   print("go")
-elif(light=="red"):
-   print("stop")   
-elif(light=="yellow"):
-   print("look")   
-else :
-   print("light broken")
-   
+if(a>b and a>c):
+    print("a is the largest")
+elif(b>a and b>c):
+    print("b is the largest")
+elif(c>a and c>b):
+    print("c is the largest")
+else:
+    print("end")           
