5a594bef27a23959366d24a2374d206fcc1c06b9
Manasvi <manasvigupta975@gmail.com>
2026-09-23 17:53:52 +0530

    tup

---
+++ tup_append.py
@@ -0,0 +1,14 @@
+movie=[]
+mov_1=input("1 fav movie = ")
+mov_2=input("2 fav movie =  ")
+mov_3=input("3 fav movie =  ")
+movie.append(mov_1)
+movie.append(mov_2)
+movie.append(mov_3)
+
+print(movie)
+
+
+
+
+
